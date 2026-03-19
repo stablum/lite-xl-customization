e3e950f61f24c020be2732a7291afb305b775c07
Foobar <foobar@example.org>
2026-03-20 00:41:07 +0100

    colors updated

--- redblack_style.lua
+++ redblack_style.lua
@@ -2,13 +2,29 @@
 
 local style = require "core.style"
 local common = require "core.common"
+local config = require "core.config"
 
 style.background = { common.color "#000000" }
 style.background2 = { common.color "#000000" }
 style.background3 = { common.color "#000000" }
-style.line_highlight = { common.color "#0b0b0b" }
+style.line_highlight = { common.color "#0b0b40" }
 style.selection = { common.color "#400a0a" }
 style.caret = { common.color "#ff0000" }
 style.line_number = { common.color "#773300" }
 style.line_number2 = { common.color "#ff0000" }
 style.caret_width = 4
+
+config.plugins.recentdirs_panel = {
+  path_prefix_color = { common.color "#553333" },
+  path_suffix_color = { common.color "#662222" },
+  hover_path_prefix_color = { common.color "#773300" },
+  hover_path_suffix_color = { common.color "#ff0000" },
+}
+
+config.plugins.recentfiles_panel = {
+  path_prefix_color = { common.color "#553333" },
+  path_suffix_color = { common.color "#662222" },
+  hover_path_prefix_color = { common.color "#773300" },
+  hover_path_suffix_color = { common.color "#ff0000" },
+}
+
