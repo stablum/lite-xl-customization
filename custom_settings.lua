ec836f65487fc206754984affdac827dd97658af
Foobar <foobar@example.org>
2026-03-20 02:37:35 +0100

    0.2.13 Add panel sort and extension color settings

--- custom_settings.lua
+++ custom_settings.lua
@@ -6,8 +6,12 @@ config.plugins.autoreload = config.plugins.autoreload or {}
 config.plugins.autoreload.always_show_nagview = false
 
 config.plugins.recentfiles_panel = config.plugins.recentfiles_panel or {}
+config.plugins.recentdirs_panel = config.plugins.recentdirs_panel or {}
 config.plugins.recentfiles_panel.visible = true
 config.plugins.recentfiles_panel.max_visible_items = 12
+
+config.plugins.recentdirs_panel.sort = true
+config.plugins.recentfiles_panel.sort = false
 
 local recentfiles_state = rawget(_G, "__recentfiles_panel_state")
 if recentfiles_state and recentfiles_state.view then
--- redblack_style.lua
+++ redblack_style.lua
@@ -14,17 +14,18 @@ style.line_number = { common.color "#773300" }
 style.line_number2 = { common.color "#ff0000" }
 style.caret_width = 4
 
-config.plugins.recentdirs_panel = {
+config.plugins.recentdirs_panel = common.merge(config.plugins.recentdirs_panel or {}, {
   path_prefix_color = { common.color "#553333" },
   path_suffix_color = { common.color "#662222" },
   hover_path_prefix_color = { common.color "#773300" },
   hover_path_suffix_color = { common.color "#ff0000" },
-}
+})
 
-config.plugins.recentfiles_panel = {
+config.plugins.recentfiles_panel = common.merge(config.plugins.recentfiles_panel or {}, {
   path_prefix_color = { common.color "#553333" },
   path_suffix_color = { common.color "#662222" },
   hover_path_prefix_color = { common.color "#773300" },
   hover_path_suffix_color = { common.color "#ff0000" },
-}
-
+  extension_color = { common.color "#0000ff" },
+  hover_extension_color = { common.color "#aaaaff" },
+})
